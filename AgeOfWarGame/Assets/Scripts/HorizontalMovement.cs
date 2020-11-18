7ce5ac32ced134e1e114098850236326eaa17b34
Thommynator <Thommynator@users.noreply.github.com>
2020-11-18 21:40:50 +0100

    Map & Buildings
    * Added first draft of player & enemy castle
    * Added map tiles
    * Added background tiles

--- AgeOfWarGame/Assets/Scripts/HorizontalMovement.cs
+++ AgeOfWarGame/Assets/Scripts/HorizontalMovement.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class HorizontalMovement : MonoBehaviour
 {
-
     public int boundary;
     public int moveSpeed;
+    public float leftLimit;
+    public float rightLimit;
 
     private void Update()
     {
 
-        if (Input.mousePosition.x > Screen.width - boundary)
+        // move right
+        if (transform.position.x < rightLimit && Input.mousePosition.x > Screen.width - boundary)
         {
             transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         }
 
-        if (Input.mousePosition.x < boundary)
+        // move left
+        if (transform.position.x > leftLimit && Input.mousePosition.x < boundary)
         {
             transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         }
