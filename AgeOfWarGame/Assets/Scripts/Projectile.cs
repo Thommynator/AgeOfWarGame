96f55cbfec8431660f5ce61160a96046091bb12c
Thommynator <Thommynator@users.noreply.github.com>
2020-12-02 22:45:11 +0100

    Special attack kills enemies

---
+++ AgeOfWarGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.gameObject.name);
+        if (collision.collider.gameObject.tag == "EnemySoldier")
+        {
+            collision.collider.gameObject.GetComponent<CurrentStats>().TakeDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
