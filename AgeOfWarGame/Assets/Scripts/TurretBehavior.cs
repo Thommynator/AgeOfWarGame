2690b96e68e416b553971f3a0cf87fcedb42953a
Thommynator <Thommynator@users.noreply.github.com>
2020-12-18 18:43:18 +0100

    Turrets target enemies and deal damage

--- AgeOfWarGame/Assets/Scripts/TurretBehavior.cs
+++ AgeOfWarGame/Assets/Scripts/TurretBehavior.cs
@@ -16,19 +16,39 @@ public class TurretBehavior : MonoBehaviour
     {
         if (Time.time - this.timeOfPreviousAttack > this.turretConfig.attackCooldown)
         {
-            GameObject projectile = Instantiate(this.turretConfig.projectile, this.transform);
-            projectile.transform.SetParent(null);
-            projectile.GetComponent<ProjectileAttack>().AttackPosition(new Vector2(-10, -2), this.turretConfig.strength);
-            this.timeOfPreviousAttack = Time.time;
-        };
+            List<Collider2D> enemieColliders = FindAllEnemiesInRange();
+            if (enemieColliders.Count > 0)
+            {
+                if (this.turretConfig.canAttackMultiple)
+                {
+                    enemieColliders.ForEach(AttackColliderPosition);
+                }
+                else
+                {
+                    // TODO the first one is not always the closest one... not sure if I want that
+                    AttackColliderPosition(enemieColliders[0]);
+                }
+                this.timeOfPreviousAttack = Time.time;
+            }
+        }
+    }
+
+    private void AttackColliderPosition(Collider2D collider)
+    {
+        GameObject projectile = Instantiate(this.turretConfig.projectile, this.transform);
+        projectile.transform.SetParent(null);
+        projectile.GetComponent<ProjectileAttack>().AttackPosition(collider.gameObject.transform.position, this.turretConfig.strength);
     }
 
 
     private List<Collider2D> FindAllEnemiesInRange()
     {
         List<Collider2D> enemieColliders = new List<Collider2D>();
-        // Physics2D.OverlapCircle(this.transform.position, this.turretConfig.attackRange, );
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(LayerMask.GetMask(new string[] { "EnemySoldier" }));
+        Physics2D.OverlapCircle(this.transform.position, this.turretConfig.attackRange, contactFilter, enemieColliders);
 
+        Debug.Log("Enemies: " + enemieColliders.Count);
         return enemieColliders;
     }
 
