0de0ad5d8f6a3f39e56e6d0bea3117e72942e985
Thommynator <Thommynator@users.noreply.github.com>
2020-12-22 15:49:47 +0100

    Added "decrease XP" animation

--- AgeOfWarGame/Assets/Scripts/XpManager.cs
+++ AgeOfWarGame/Assets/Scripts/XpManager.cs
@@ -4,10 +4,16 @@ public class XpManager : MonoBehaviour
 {
     public int xp;
 
+    public ParticleSystem decreaseXpPS;
+
     void Start()
     {
         GameEvents.current.onIncreaseXp += (int newXp) => this.xp += newXp;
-        GameEvents.current.onDecreaseXp += (int newXp) => this.xp -= newXp;
+        GameEvents.current.onDecreaseXp += (int newXp) =>
+        {
+            this.xp -= newXp;
+            this.decreaseXpPS.Play();
+        };
     }
 
 }
