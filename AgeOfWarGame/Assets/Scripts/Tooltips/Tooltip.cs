a7395769b552150d16fbd7b5161770d4978b192b
Thommynator <Thommynator@users.noreply.github.com>
2021-01-16 19:28:39 +0100

    Show tooltip with offset
    This way the mouse doesn't block the view on the tooltip. Better readability.
    The offset depends on the position on the screen, e.g.
    * top-left screen: tooltip is below & right of mouse
    * bottom-right screen: tooltip is above & left of mouse
    * ...

--- AgeOfWarGame/Assets/Scripts/Tooltips/Tooltip.cs
+++ AgeOfWarGame/Assets/Scripts/Tooltips/Tooltip.cs
@@ -10,10 +10,39 @@ public class Tooltip : MonoBehaviour
     void Update()
     {
         Vector2 mousePosition = Input.mousePosition;
-        float pivotX = mousePosition.x / Screen.width;
-        float pivotY = mousePosition.y / Screen.height;
-        GetComponent<RectTransform>().pivot = new Vector2(pivotX, pivotY);
-        this.transform.position = mousePosition + Vector2.down * 80;
+
+        GetComponent<RectTransform>().pivot = GetPivot(mousePosition);
+        this.transform.position = mousePosition;
+    }
+
+    private Vector2 GetPivot(Vector2 mousePosition)
+    {
+
+        float mx = mousePosition.x;
+        float my = mousePosition.y;
+
+        float w = Screen.width;
+        float h = Screen.height;
+
+        float offset = 0.2f;
+
+        if (mx >= 0 && mx < w / 2 && my >= h / 2 && my <= h)
+        {
+            return new Vector2(0, 1 + offset);
+        }
+        else if (mx >= w / 2 && mx <= w && my >= h / 2 && my <= h)
+        {
+            return new Vector2(1, 1 + offset);
+        }
+        else if (mx >= 0 && mx < w / 2 && my >= 0 && my < h / 2)
+        {
+            return new Vector2(0, 0 - offset);
+        }
+        else
+        {
+            return new Vector2(1, 0 - offset);
+        }
+
     }
 
 
