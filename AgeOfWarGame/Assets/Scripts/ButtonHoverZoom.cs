61af3028d7db2bcc6876d8308ccbc6a3290e7d9a
Thommynator <Thommynator@users.noreply.github.com>
2021-02-27 18:19:38 +0100

    Menu screen

---
+++ AgeOfWarGame/Assets/Scripts/ButtonHoverZoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverZoom : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    private LTDescr loopAnimation;
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        this.loopAnimation = LeanTween.scale(this.gameObject, Vector3.one * 1.1f, 0.3f).setLoopPingPong();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        LeanTween.cancel(this.loopAnimation.id);
+        LeanTween.scale(this.gameObject, Vector3.one, 0.3f);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ AgeOfWarGame/Assets/Scripts/TitleShake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleShake : MonoBehaviour {
+
+    public float duration;
+    public float rotationAngle;
+
+    // Start is called before the first frame update
+    void Start() {
+        LeanTween.scale(this.gameObject, 1.1f * Vector3.one, duration).setLoopPingPong();
+        LeanTween.rotateZ(this.gameObject, this.rotationAngle, 2 * duration).setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
